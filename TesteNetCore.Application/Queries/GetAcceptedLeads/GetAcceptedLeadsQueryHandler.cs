ce63360ddddbfb45d8c866d84803e0f58f6cdde8
rafael.mesquita <rafael.mesquita@animaeducacao.com.br>
2024-01-22 15:57:35 -0300

    queries de busca de leads aceitos

---
+++ TesteNetCore.Application/Queries/GetAcceptedLeads/GetAcceptedLeadsQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using TesteNetCore.Application.Mapper;
+using TesteNetCore.Domain.Enum;
+using TesteNetCore.Domain.Repository.Interface;
+
+namespace TesteNetCore.Application.Queries.GetLeads
+{
+    public class GetAcceptedLeadsQueryHandler : IRequestHandler<GetAcceptedLeadsQuery, List<GetAcceptedLeadsViewModel>>
+    {
+        private readonly ILeadRepository _repository;
+        private readonly IObjectConverter _mapper;
+
+        public GetAcceptedLeadsQueryHandler(ILeadRepository repository, IObjectConverter mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        public async Task<List<GetAcceptedLeadsViewModel>> Handle(GetAcceptedLeadsQuery request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                List<GetAcceptedLeadsViewModel> leads = _mapper.Map<List<GetAcceptedLeadsViewModel>>(await _repository.GetLeads()).Where(x=>x.StatusLeadId == LeadStatus.Accepted).ToList();
+                return await Task.FromResult(leads);
+
+            }catch(Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ TesteNetCore.Application/Queries/GetAcceptedLeads/GetAcceptedLeadsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using TesteNetCore.Domain.Enum;
+
+namespace TesteNetCore.Application.Queries.GetLeads
+{
+    public class GetAcceptedLeadsViewModel
+    {
+        public string ContactFirstName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Suburb { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Id { get; set; }
+        public LeadStatus StatusLeadId { get; set; }
+        public string ContactFullName { get; set; }
+        public string ContactPhoneNumber { get; set; }
+        public string ContactEmail { get; set; }
+
+    }
+}
