eefaba08b8c449c5899faa7cec4aac544ee4f765
rafael.mesquita <rafael.mesquita@animaeducacao.com.br>
2024-01-22 15:58:41 -0300

    Adicionando enum de status

---
+++ TesteNetCore.Domain/Enum/LeadStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteNetCore.Domain.Enum
+{
+    public enum LeadStatus
+    {
+        Accepted = 1,
+        Declined = 2,
+        Pending = 3
+    }
+}
